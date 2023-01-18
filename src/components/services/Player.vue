a52c4af1b21c898af98ac662a8e2e9e423f3132c
Daniel David Sansano Surla <dandavesan@gmail.com>
2023-01-18 23:46:43 +1100

    init first files

---
+++ src/components/services/Player.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+
+export default defineComponent({
+  components: {
+    VideoPlayer
+  }
+})
+</script>
+
+<template>
+  <video-player poster="@/resources/poster.jpg" :loop="false" :volume="0.5" />
+</template>
---
+++ src/views/PlayerView.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import VueVideoPlayer from '@/components/services/Player.vue'
+
+export default {
+  name: 'VideoExample',
+  components: {
+    VueVideoPlayer
+  },
+  data () {
+    return {
+      videoOptions: {
+        autoplay: false,
+        controls: true,
+        sources: [
+          {
+            src: '/resources/user/video.webm',
+            type: 'video/webm'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
