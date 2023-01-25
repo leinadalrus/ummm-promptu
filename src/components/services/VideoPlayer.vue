96e94376a1fe1d4b5b6dbb4c0762c739b8cde7b8
Daniel David Sansano Surla <dandavesan@gmail.com>
2023-01-25 13:25:19 +1100

    added tailwind-css

--- src/components/services/VideoPlayer.vue
+++ src/components/services/VideoPlayer.vue
@@ -12,23 +12,30 @@ export default defineComponent({
 
 <!-- have database connection value of API URL here -->
 <template>
-  <video-player
-    :children="[]"
-    src=""
-    post=""
-    controls
-    :loop="false"
-    :volume="0.5"
+  <main
+    class="w-640 md:-800 lg:1280 bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
   >
-    <template v-slot="{ player, state }">
-      <div class="custom-player-controls">
-        <button @click="state.playing ? player.pause() : player.play()">
-          {{ state.playing ? 'Pause' : 'Play' }}
-        </button>
-        <button @click="player.muted(!state.muted)">
-          {{ state.muted ? 'UnMute' : 'Mute' }}
-        </button>
-      </div>
-    </template>
-  </video-player>
+    <video-player
+      :children="[]"
+      src=""
+      post=""
+      controls
+      :loop="false"
+      :volume="0.5"
+    >
+      <template v-slot="{ player, state }">
+        <div class="custom-player-controls">
+          <button @click="state.playing ? player.pause() : player.play()">
+            {{ state.playing ? 'Pause' : 'Play' }}
+          </button>
+          <button @click="player.muted(!state.muted)">
+            {{ state.muted ? 'UnMute' : 'Mute' }}
+          </button>
+        </div>
+      </template>
+    </video-player>
+    <article
+      class="max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl bg-white dark:bg-black"
+    ></article>
+  </main>
 </template>
