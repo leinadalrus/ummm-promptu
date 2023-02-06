05aa97b0582c16bf30ebdafbe2f45b91c98acf97
Daniel David Sansano Surla <dandavesan@gmail.com>
2023-02-06 18:29:44 +1100

    updated environments

--- src/components/processors/VideosCollectionProcessor.vue
+++ src/components/processors/VideosCollectionProcessor.vue
@@ -5,21 +5,38 @@ import { onMounted } from 'vue'
 onMounted(() => {})
 
 const observedItem = ref('videosCollection') // reference GraphQL query
+import { supabase } from '../client/supabaseClient'
+
+// Initialize the JS client
+const SupabasePostgresUri = process.env.SUPABASE_POSTGRES_URI?.toString()
 
 watch(
   observedItem,
   async (incomingEvent, outgoingEvent) => {
     // console args value is-equal-to 2
     if (incomingEvent.lastIndexOf('2')) {
-      let response = await fetch('') // fetch graphql api url
-      observedItem.value = (await response.json()).value
+      let response = await fetch(
+        SupabasePostgresUri + '/videosCollection/${videos.value}'
+      ) // fetch graphql api url VIP(Replace): replace 'todo' values
+      observedItem.value = await response.json()
     }
   },
   { immediate: true }
 )
 
+// SUBSCRIBE TO ALL EVENTS
+const videosCollection = supabase
+  .channel('custom-all-channel')
+  .on(
+    'postgres_changes',
+    { event: '*', schema: 'public', table: 'videosCollection' },
+    payload => {
+      console.log('Change received!', payload)
+    }
+  )
+  .subscribe()
+
 export const storeVideos = reactive({
-  video: {},
+  videosCollection: { videosCollection }
 })
 </script>
-
