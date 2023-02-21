3f9f0213d0eb1e938d31859689a6effd31e1ba5a
David <daniel.david.surla@gmail.com>
2023-02-21 01:26:24 +0000

    updated StreamScraper.vue

--- src/components/models/StreamScraper.vue
+++ src/components/models/StreamScraper.vue
@@ -1,6 +1,12 @@
 <script lang="ts">
+'use strict'
+
+import * as fs from 'fs'
+import * as path from 'path'
 import { ifError } from 'assert'
 import { reactive, ref } from 'vue'
+import { assert } from 'vitest'
+const papa = require('papaparse')
 
 export default {
   // `setup` is a special hook dedicated for composition API.
@@ -35,10 +41,39 @@ export default {
       return sourceName
     }
 
+    function promiseCommas(filename: string) {
+      return new Promise((ok) => {
+        fs.readFile(filename, 'utf-8', function (err, data) {
+          if (err) ifError(err)
+          ok(data)
+        })
+      })
+    }
+
+    function resolveCommas(filepath: string) {
+      return promiseCommas(filepath)
+        .then((data) => {
+          const result = papa.parse(data, {
+            header: true,
+            dynamicTyping: true,
+          })
+          return result.data
+        })
+        .catch((err) => {
+          console.table(err.stack)
+        })
+    }
+
+    assert(
+      crawlScrapeFrom(sourceName!) === changeStream(sourceName!),
+      '`sourceName` should be decide all-round for both functions',
+    )
+
     // expose the state to the template
     return {
       videoSources,
       changeStream,
+      resolveCommas,
     }
   },
 }
